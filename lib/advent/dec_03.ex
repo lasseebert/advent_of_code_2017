95191a77678814b586b61b4355806d0f01424c9a
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2017-12-03 22:02:26 +0100

    Day 3, part 2

--- lib/advent/dec_03.ex
+++ lib/advent/dec_03.ex
@@ -22,6 +22,28 @@ defmodule Advent.Dec03 do
   How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?
 
   Your puzzle input is 312051.
+
+  --- Part Two ---
+
+  As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1. Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
+
+  So, the first few squares' values are chosen as follows:
+
+  Square 1 starts with the value 1.
+  Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+  Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+  Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+  Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+  Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
+
+  147  142  133  122   59
+  304    5    4    2   57
+  330   10    1    1   54
+  351   11   23   25   26
+  362  747  806--->   ...
+  What is the first value written that is larger than your puzzle input?
+
+  Your puzzle input is still 312051.
   """
 
   @default_square 312051
@@ -29,6 +51,11 @@ defmodule Advent.Dec03 do
   def run(n \\ @default_square) do
     {x, y} = find_coords(n)
     abs(x) + abs(y)
+  end
+
+  def run_2(n \\ @default_square) do
+    sum_squares()
+    |> Enum.find(fn k -> k > n end)
   end
 
   defp find_coords(n) do
@@ -51,4 +78,29 @@ defmodule Advent.Dec03 do
   defp next_step({count, :up}), do: {count + 1, :left}
   defp next_step({count, :left}), do: {count, :down}
   defp next_step({count, :down}), do: {count + 1, :right}
+
+  defp sum_squares do
+    Stream.unfold({%{}, 1, {0, 0}, {0, :no_dir}, {1, :right}}, &next_sum_square/1)
+  end
+
+  defp next_sum_square({grid, n, coords, {0, _dir}, step}) do
+    next_sum_square({grid, n, coords, step, next_step(step)})
+  end
+  defp next_sum_square({grid, n, coords, {count, dir}, step}) do
+    grid = Map.put(grid, coords, n)
+    {x, y} = n_coords = next_coords(coords, {1, dir})
+    next_n = [
+      {x - 1, y - 1},
+      {x - 1, y},
+      {x - 1, y + 1},
+      {x, y - 1},
+      {x, y + 1},
+      {x + 1, y - 1},
+      {x + 1, y},
+      {x + 1, y + 1}
+    ]
+    |> Enum.map(&Map.get(grid, &1, 0))
+    |> Enum.sum
+    {n, {grid, next_n, n_coords, {count - 1, dir}, step}}
+  end
 end
--- test/dec_03_test.exs
+++ test/dec_03_test.exs
@@ -3,19 +3,39 @@ defmodule Advent.Dec03Test do
 
   alias Advent.Dec03
 
-  test "already at access port" do
-    assert Dec03.run(1) == 0
+  describe "part 1" do
+    test "already at access port" do
+      assert Dec03.run(1) == 0
+    end
+
+    test "12" do
+      assert Dec03.run(12) == 3
+    end
+
+    test "23" do
+      assert Dec03.run(23) == 2
+    end
+
+    test "1024" do
+      assert Dec03.run(1024) == 31
+    end
   end
 
-  test "12" do
-    assert Dec03.run(12) == 3
-  end
+  describe "part 2" do
+    test "1" do
+      assert Dec03.run_2(1) == 2
+    end
 
-  test "23" do
-    assert Dec03.run(23) == 2
-  end
+    test "2" do
+      assert Dec03.run_2(2) == 4
+    end
 
-  test "1024" do
-    assert Dec03.run(1024) == 31
+    test "7" do
+      assert Dec03.run_2(7) == 10
+    end
+
+    test "500" do
+      assert Dec03.run_2(500) == 747
+    end
   end
 end
