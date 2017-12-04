75de6634d5bd75fdff7f7b20d23460337faeb37c
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2017-12-04 08:45:55 +0100

    Day 4, part 2

--- lib/advent/dec_04.ex
+++ lib/advent/dec_04.ex
@@ -12,6 +12,19 @@ defmodule Advent.Dec04 do
   aa bb cc dd aa is not valid - the word aa appears more than once.
   aa bb cc dd aaa is valid - aa and aaa count as different words.
   The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+
+  --- Part Two ---
+
+  For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+  For example:
+
+  abcde fghij is a valid passphrase.
+  abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+  a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+  iiii oiii ooii oooi oooo is valid.
+  oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+  Under this new system policy, how many passphrases are valid?
   """
 
   @default_input_path "inputs/dec_04"
@@ -20,17 +33,40 @@ defmodule Advent.Dec04 do
 
   def count_valid_passphrases(input \\ @default_input) do
     input
-    |> String.trim
-    |> String.split(~r/\n/)
+    |> parse_input
     |> Enum.count(&valid_passphrase?/1)
+  end
+
+  def count_valid_strong_passphrases(input \\ @default_input) do
+    input
+    |> parse_input
+    |> Enum.count(&valid_strong_passphrase?/1)
   end
 
   def valid_passphrase?(passphrase) do
     passphrase
     |> String.split(~r/\W+/)
+    |> unique_words?
+  end
+
+  def valid_strong_passphrase?(passphrase) do
+    passphrase
+    |> String.split(~r/\W+/)
+    |> Enum.map(fn word -> word |> String.graphemes |> Enum.sort |> Enum.join end)
+    |> unique_words?
+  end
+
+  defp unique_words?(words) do
+    words
     |> Enum.sort
     |> Stream.chunk_every(2, 1)
     |> Enum.find(&match?([a, a], &1))
     |> is_nil
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.trim
+    |> String.split(~r/\n/)
   end
 end
--- test/dec_04_test.exs
+++ test/dec_04_test.exs
@@ -25,4 +25,38 @@ defmodule Advent.Dec04Test do
       assert Dec04.count_valid_passphrases(passphrases) == 2
     end
   end
+
+  describe "part 2" do
+    test "valid passphrase" do
+      assert Dec04.valid_strong_passphrase?("abcde fghij") == true
+    end
+
+    test "invalid passphrase" do
+      assert Dec04.valid_strong_passphrase?("abcde xyz ecdab") == false
+    end
+
+    test "valid passphrase with almost same words" do
+      assert Dec04.valid_strong_passphrase?("a ab abc abd abf abj") == true
+    end
+
+    test "valid passphrase with same letters repeated different number of times" do
+      assert Dec04.valid_strong_passphrase?("iiii oiii ooii oooi oooo") == true
+    end
+
+    test "invalid passphrase with same word rearranged" do
+      assert Dec04.valid_strong_passphrase?("oiii ioii iioi iiio") == false
+    end
+
+    test "counting strong passphrases" do
+      passphrases = """
+      abcde fghij
+      abcde xyz ecdab
+      a ab abc abd abf abj
+      iiii oiii ooii oooi oooo
+      oiii ioii iioi iiio
+      """
+
+      assert Dec04.count_valid_strong_passphrases(passphrases) == 3
+    end
+  end
 end
