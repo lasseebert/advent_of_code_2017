ec40d85dec0489a681dd5bb493473d3b6bc02d1d
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2017-12-16 23:03:08 +0100

    Day 16 part 2

--- lib/advent/dec_16.ex
+++ lib/advent/dec_16.ex
@@ -19,6 +19,19 @@ defmodule Advent.Dec16 do
   After finishing their dance, the programs end up in order baedc.
 
   You watch the dance for a while and record their dance moves (your puzzle input). In what order are the programs standing after their dance?
+
+  --- Part Two ---
+
+  Now that you're starting to get a feel for the dance moves, you turn your attention to the dance as a whole.
+
+  Keeping the positions they ended up in from their previous dance, the programs perform it again and again: including the first dance, a total of one billion (1000000000) times.
+
+  In the example above, their second dance would begin with the order baedc, and use the same dance moves:
+
+  s1, a spin of size 1: cbaed.
+  x3/4, swapping the last two programs: cbade.
+  pe/b, swapping programs e and b: ceadb.
+  In what order are the programs standing after their billion dances?
   """
 
   @default_input_path "inputs/dec_16"
@@ -32,11 +45,33 @@ defmodule Advent.Dec16 do
     |> Enum.reduce(programs, &single_move(&2, &1))
   end
 
+  def dance_a_lot(programs \\ @default_programs, moves \\ parse_moves(@default_input)) do
+    repeat_dance(programs, moves, 0, %{})
+  end
+
   def parse_moves(input) do
     input
     |> String.trim
     |> String.split(",")
     |> Enum.map(&parse_move/1)
+  end
+
+  defp repeat_dance(programs, moves, count, seen) do
+    if Map.has_key?(seen, programs) do
+      loop_start = Map.get(seen, programs)
+      loop_end = count
+      loop_size = loop_end - loop_start
+      remainder = rem(1_000_000_000 - loop_end, loop_size)
+      1..remainder
+      |> Enum.reduce(programs, fn _, programs -> dance(programs, moves) end)
+    else
+      repeat_dance(
+        dance(programs, moves),
+        moves,
+        count + 1,
+        Map.put(seen, programs, count)
+      )
+    end
   end
 
   defp single_move(programs, {:spin, size}) do
